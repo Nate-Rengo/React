9a134a6f30d9c6d8b7d7e5d073f3c649e70411a9
Nate Rengo <naterengo@gmail.com>
2025-03-09 15:35:42 -0500

    react changes

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import '../App.css';
+const Header = () => (
+  <header>
+    <h1>Nate's React Website!</h1>
+  </header>
+);
+
+export default Header;
---
+++ src/components/MainContent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../App.css';
+const MainContent = () => (
+  <main>
+    <section>
+      <h2>Welcome to the Website!</h2>
+    </section>
+  </main>
+);
+
+export default MainContent;
